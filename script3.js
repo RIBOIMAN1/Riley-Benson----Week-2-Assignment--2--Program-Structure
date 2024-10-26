3b19d61d6d270c5f14f9b65b33766290d69887d5
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-10-26 03:57:16 -0700

    script3.js: - Added logic that displays the grid and makes it have 8x8 dimensions. - Made a for loop that goes from 0 to 7 (8 values) to create an alternating pattern horizontally. - Associated "rowStr" with the console. - Used a tag that displays the grid to the console.

--- script3.js
+++ script3.js
@@ -9,3 +9,21 @@
 //  # # # #
 // # # # #
 //  # # # #
+function Grid() {
+    const size = 8; // Logic for displaying grid with the dimensions of the grid being 8x8
+  
+    for (let row = 0; row < size; row++) {
+      let rowStr = ''; // For loop that iterates over each row of the grid, going up to a value of 7 (from 0 since there are 8 values)
+  
+      for (let col = 0; col < size; col++) {
+        if ((row + col) % 2 === 0) {
+          rowStr += '#';
+        } else {
+          rowStr += ' ';
+        } // For loop going from 0 to 7, putting even values and odd values in different spots to create the alternating pattern
+      }
+      
+      console.log(rowStr);
+    } // Associates "rowStr" to the console
+}
+Grid(); // Displays the patterned grid to the console
