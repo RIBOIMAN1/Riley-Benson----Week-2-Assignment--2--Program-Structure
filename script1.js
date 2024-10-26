ef2f85a0805c60970186d570e567bdeb726d1e74
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-10-25 22:37:34 -0700

    script1.js: - Created commands that set the maximum number of rows for the triangle to 4. - Created commands that increments a value, "i" by 1 until it hits a value of 4. - Created a similar set of commands that instead descend the "i" value after it has reached a value of 4. - Created a command that displays the created triangle.

--- script1.js
+++ script1.js
@@ -7,3 +7,16 @@
 // ###
 // ##
 // #
+function displayTriangle() {
+    const maxRows = 4; // Logic to display a triangle to the console with the max rows set to 4
+  
+    for (let i = 1; i <= maxRows; i++) {
+      console.log('#'.repeat(i));
+    } // Increments i until it is less than or equal to maxRows, then repeats the "#" 4 times (represented by "i")
+  
+    for (let i = maxRows - 1; i >= 1; i--) {
+      console.log('#'.repeat(i));
+    }
+} // Similar to the previous set of commands, but for descending the amount of "#" outputted to each row of the console
+  
+displayTriangle(); // Displays the hashtag triangle shape to the console
